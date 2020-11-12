3bf996227d51bf376865289b8b6879b5c8941660
tsithole <tsithole@student.wethinkcode.co.za>
2020-11-12 13:34:04 +0200

    updated

--- interface.py
+++ interface.py
@@ -3,21 +3,71 @@ import json
 import os
 
 
+username = ''
+
 def create_profile():
-    name = ''
-    if not os.path.exists("config.json"):
-        while name == '':
-            name = input("Please enter student username: ")
-            campus = input("Which campus are you from: ")
-            email = name + "@student.wethinkcode.co.za"
-        with open("config.json","w") as person:
-            json.dump({"email":email,"campus":campus},person)
+    """
+    This funtion creates the users config file if one doesnt exist and 
+    file path if one exist
+    """
+    global username
+    username = input("Please enter student username: ")
+    while not os.path.exists(f"{username}.json"):
+    # while username == '':
+        # username = input("Please enter student username: ")
+        campus = input("Which campus are you from: ")
+        email = username + "@student.wethinkcode.co.za"
+        pword1 = input("please enter password: ")
+        pword2 = input("please confirm password: ")
+
+        while pword1 != pword2:
+            print("password don't match : ")
+            pword1 = input("please enter password: ")
+            pword2 = input("please confirm password: ")
+            
+        with open(f"{username}.json","w") as person:
+            json.dump({"username":username,"email":email,"campus":campus, "password": password_hasher(pword1, pword2)},person)
             return "Profile Created"
     else:
-        with open("config.json") as person:
+        with open(f"{username}.json","r") as person:
             file1  = json.load(person)
             print('configuration file exists:\n', '~/.config/clinix/config.json')
             return file1
 
 
-print(create_profile())
+def password_hasher(pword1, pword2):
+    """
+    This funtion will encrypt the password
+    """
+    return pword1
+
+
+def password_validator(pword1, file1):
+    """
+    This funtion vaildates the pasword
+    """
+    input_pass = password_hasher(pword1, pword1)
+    return input_pass == file1
+
+"TODO check pickle's expiry"
+def get_user_info():
+    """
+    
+    """
+    global username
+    username = input("Please enter student username: ")
+    if os.path.exists(f"{username}.json"):
+        with open(f"{username}.json", "r") as person:
+            person_info = json.loads(person.read())
+            if password_validator(input('Please enter password to login '), person_info["password"]):
+                if os.path.exists(f'{username}.pickle'):
+                    print("Token expiry: ")
+                    return True, person_info
+                else:
+                    return True, person_info
+            else:
+                return False, "Incorrect password"
+    else:
+        print("Use 'clinix init'")
+        exit()
+# print(create_profile())
