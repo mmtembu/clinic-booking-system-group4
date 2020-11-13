edeb6e4bb5b0102dd3db92277ec6611446fc9398
mmtembu <mmtembu@student.wethinkcode.co.za>
2020-11-13 12:26:34 +0200

    Working iteration v1

---
+++ clinix-api-start.py
@@ -0,0 +1,58 @@
+
+import sys
+from interface import create_profile,  get_user_info, is_logged_in, logout
+from calendar_sync import *
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+
+    get_calendars()
+
+#   CLINICIANS    -   allows the student to view all the available clinicians
+#   CLINIX        -   shows coding clinix calendar events
+#   BOOKING       -   allows a student to make a booking to an available slot
+
+
+def do_help():
+    """Lists all the available commands"""
+    print("""
+HELP          -   lists all the available commands the booking system provides
+LOGIN         -   initializes the clinix calendar
+LOGOUT        -   use this command to remove your credentials from current
+system
+VIEW_CALENDAR -   shows list of calendars you can view
+""")
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        if sys.argv[1].upper() == 'HELP':
+            do_help()
+        elif sys.argv[1].upper() == 'INIT':
+            create_profile()
+        elif sys.argv[1].upper() == 'LOGIN':
+            main()
+        elif sys.argv[1].upper() == 'LOGOUT':
+            logout()
+        elif sys.argv[1].upper() == 'VIEW_CALENDAR':
+            print('Which calendar do you want?\n1. Your calendar\n2. Clinix Calendar\n')
+            num = input('which calendar do you want?[choose number] ')
+            print()
+            while not num.isnumeric() or int(num) > 2 or int(num) < 1:
+                num = input('which calendar do you want?[choose number] ')
+            if num == '1':
+                read_data('student')
+            elif num == '2':
+                read_data('clinix')
+            print()
+        else:
+            print('Please login')
+    elif len(sys.argv) == 1:
+        do_help()
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+error_function(){
+	echo "$1" #1>&2
+	# exit 1
+}
+
+# This welcomes the user to the system and the creates the 'clinix' directory.
+# If it exists then it throws an error
+
+echo ' '
+
+echo '     ___       __    _______    ___        ________   ________   _____ ______    _______           _________   ________         
+    |\  \     |\  \ |\  ___ \  |\  \      |\   ____\ |\   __  \ |\   _ \  _   \ |\  ___ \         |\___   ___\|\   __  \        
+    \ \  \    \ \  \\ \   __/| \ \  \     \ \  \___| \ \  \|\  \\ \  \\\__\ \  \\ \   __/|        \|___ \  \_|\ \  \|\  \       
+     \ \  \  __\ \  \\ \  \_|/__\ \  \     \ \  \     \ \  \\\  \\ \  \\|__| \  \\ \  \_|/__           \ \  \  \ \  \\\  \      
+      \ \  \|\__\_\  \\ \  \_|\ \\ \  \____ \ \  \____ \ \  \\\  \\ \  \    \ \  \\ \  \_|\ \           \ \  \  \ \  \\\  \     
+       \ \____________\\ \_______\\ \_______\\ \_______\\ \_______\\ \__\    \ \__\\ \_______\           \ \__\  \ \_______\    
+        \|____________| \|_______| \|_______| \|_______| \|_______| \|__|     \|__| \|_______|            \|__|   \|_______|    
+     ________   ________   ________   _______           ________   ___        ___   ________    ___      ___    ___             
+    |\   ____\ |\   __  \ |\   ___ \ |\  ___ \         |\   ____\ |\  \      |\  \ |\   ___  \ |\  \    |\  \  /  /|            
+    \ \  \___| \ \  \|\  \\ \  \_|\ \\ \   __/|        \ \  \___| \ \  \     \ \  \\ \  \\ \  \\ \  \   \ \  \/  / /            
+     \ \  \     \ \  \\\  \\ \  \ \\ \\ \  \_|/__       \ \  \     \ \  \     \ \  \\ \  \\ \  \\ \  \   \ \    / /             
+      \ \  \____ \ \  \\\  \\ \  \_\\ \\ \  \_|\ \       \ \  \____ \ \  \____ \ \  \\ \  \\ \  \\ \  \   /     \/              
+       \ \_______\\ \_______\\ \_______\\ \_______\       \ \_______\\ \_______\\ \__\\ \__\\ \__\\ \__\ /  /\   \              
+        \|_______| \|_______| \|_______| \|_______|        \|_______| \|_______| \|__| \|__| \|__| \|__|/__/ /\ __\             
+                                                                                                        |__|/ \|__|             
+                                                                                                                                
+                                                                                                                                '
+
+echo ' '
+
+if mkdir ~/.config/clinix; then
+	echo "Clinix directory has been created."
+else
+	error_function "Cannot create a 'clinix' directory, it already exists!"
+fi
+
+#Moves the files to the relevant directories
+if cp -f ~/Downloads/quickstart/* ~/.config/clinix/; then #change from 'cp' -> 'mv'
+
+	echo 'Relevant files have been moved.'
+	echo 'source ~/.config/clinix/clinix.sh' >> ~/.zshrc
+	chmod +x ~/.config/clinix/clinix.sh
+else
+	error_function 'Unfortunately the files cannot be moved'
+fi
+
